8fd1d9f187917ed48957fc4f58655176938c7b83
Bruno Logerfo <bclogerfo@hotmail.com>
2019-10-18 17:30:36 -0300

    initial code

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,64 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import * as github from "@actions/github";
+
+const token = core.getInput("github-token", { required: true });
+const releaseBranch = getBranch("release");
+const devBranch = getBranch("dev");
+const masterBranch = getBranch("master");
+
+function getInput(name: string, fallback: string) {
+    const input = core.getInput(name);
+    return input || fallback;
+}
+
+function getBranch(name: string): string {
+    return getInput(name, name);
+}
+
+function getTarget(head: string): string | null {
+    switch (head) {
+        case releaseBranch: return masterBranch;
+        case masterBranch: return devBranch;
+        default: return null;
+    }
+}
 
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    const context = github.context,
+        head = context.ref,
+        base = getTarget(head);
+    if (!base) {
+        return;
+    }
+    const owner = context.repo.owner,
+        repo = context.repo.repo,
+        client = new github.GitHub(token),
+        creationResponse = await client.pulls.create({
+            base,
+            head,
+            owner,
+            repo,
+            title: `${head} -> ${base}`,
+        }),
+        creationData = creationResponse.data,
+        pull_number = creationData.number;
+    await client.issues.addLabels({
+        issue_number: pull_number,
+        labels: [getInput("label", "gitflow")],
+        owner,
+        repo
+    });
+    await client.pulls.createReview({
+        event: "APPROVE",
+        owner,
+        pull_number,
+        repo,
+    });
+    await client.pulls.merge({
+        owner,
+        pull_number,
+        repo,
+    });
 }
 
 run();
