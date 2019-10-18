6b512f4d27a141cb04e509acf66562dab88048a9
Bruno Logerfo <bclogerfo@hotmail.com>
2019-10-18 17:51:26 -0300

    fix syntax

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import * as core from '@actions/core';
-import * as github from "@actions/github";
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 const token = core.getInput("github-token", { required: true });
 const releaseBranch = getBranch("release");
