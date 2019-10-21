1f0106f72df8deb24cb06d72c1b8a10cd30e1cb2
Bruno Logerfo <bclogerfo@hotmail.com>
2019-10-21 12:21:38 -0300

    pull_request event

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const token = core.getInput("github-token", { required: true });
-const releaseBranch = getBranch("release");
-const devBranch = getBranch("dev");
-const masterBranch = getBranch("master");
+const token = core.getInput("github-token", { required: true }),
+    client = new github.GitHub(token),
+    releaseBranch = getBranch("release"),
+    devBranch = getBranch("dev"),
+    masterBranch = getBranch("master"),
+    label = getInput("label", "gitflow"),
+    context = github.context,
+    owner = context.repo.owner,
+    repo = context.repo.repo;
 
 function getInput(name, fallback) {
     const input = core.getInput(name);
@@ -24,37 +29,55 @@ function getTarget(head) {
 }
 
 async function run() {
-    const context = github.context,
-        head = context.ref.substr(11),
-        base = getTarget(head);
     core.debug(JSON.stringify(context.payload));
+    switch (github.context.eventName) {
+        case "push":
+            await push();
+            break;
+
+        case "pull_request":
+            await pr();
+            break;
+    }
+}
+
+async function push() {
+    const head = context.ref.substr(11),
+        base = getTarget(head);
     if (!base) {
         core.info(`Branch ${head} is neither ${masterBranch} or ${releaseBranch}. Skipping...`);
         return;
     }
-    const owner = context.repo.owner,
-        repo = context.repo.repo,
-        client = new github.GitHub(token),
-        creationResponse = await client.pulls.create({
-            base,
-            head,
-            owner,
-            repo,
-            title: `${head} -> ${base}`,
-        }),
+    const creationResponse = await client.pulls.create({
+        base,
+        head,
+        owner,
+        repo,
+        title: `${head} -> ${base}`,
+    }),
         creationData = creationResponse.data,
         pull_number = creationData.number;
     core.info(`Pull request #${pull_number} created.`);
     core.debug(JSON.stringify(creationData));
-    const label = getInput("label", "gitflow"),
-        labelsResponse = await client.issues.addLabels({
-            issue_number: pull_number,
-            labels: [label],
-            owner,
-            repo
-        });
+    const labelsResponse = await client.issues.addLabels({
+        issue_number: pull_number,
+        labels: [label],
+        owner,
+        repo
+    });
     core.info(`Label ${label} added to #${pull_number}.`);
     core.debug(JSON.stringify(labelsResponse.data));
+    await merge(pull_number);
+}
+
+async function pr() {
+    const pull_number = context.number;
+    if (context.pull_request.labels.includes(label)) {
+        await merge(pull_number);
+    }
+}
+
+async function merge(pull_number) {
     const mergeResponse = await client.pulls.merge({
         owner,
         pull_number,
